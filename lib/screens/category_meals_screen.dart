7d64825769e6aeb47117869a5b61af3c80a57fdb
Benjamin Corben <corbenbenjamin@gmail.com>
2019-12-01 20:03:18 +0100

    Filter logic which needs to be relooked at (parsing data and functions)

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meals/widgets/meal_item.dart';
 
-import '../dummy_data.dart';
+import '../widgets/meal_item.dart';
 import '../models/meal.dart';
 
 class CategoryMealsScreen extends StatefulWidget {
   static const routeName = "/categories-meals";
+
+  final List<Meal> availableMeals;
+
+
+  CategoryMealsScreen(this.availableMeals);
 
   @override
   _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
@@ -23,7 +27,7 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
           ModalRoute.of(context).settings.arguments as Map<String, String>;
       categoryTitle = routeArgs["title"];
       final categoryId = routeArgs["id"];
-      displayedMeals = DUMMY_MEALS.where((meal) {
+      displayedMeals = widget.availableMeals.where((meal) {
         return meal.categories.contains(categoryId);
       }).toList();
       _loadedInitData = true;
